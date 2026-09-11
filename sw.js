7d811cb0096f8f11accb660d6d04fa0fd8bebf50
Lorena Torres <lts07102003@gmail.com>
2026-09-10 21:24:50 -0300

    Update service worker caching logic and names

--- sw.js
+++ sw.js
@@ -1,22 +1,15 @@
-const CACHE_NAME = 'sinalverde-v1';
-const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json'
-];
+const CACHE_NAME = 'encontro-v1';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS_TO_CACHE))
-  );
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(['./', './index.html', './style.css', './app.js', './manifest.json']);
+        })
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
 });
